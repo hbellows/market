2c8728a3a5fa2050362c205b13c7acfcf419801a
Harper Bellows <harper.bellows@gmail.com>
2018-07-31 10:39:16 -0600

    iteration 3

--- lib/market.rb
+++ lib/market.rb
@@ -1,11 +1,13 @@
 class Market
 
   attr_reader :name,
-              :vendors
+              :vendors,
+              :stocked_items
 
   def initialize(name)
     @name = name
     @vendors = []
+    @stocked_items = Hash.new(0)
   end
 
   def add_vendor(vendor)
@@ -19,4 +21,24 @@ class Market
   def vendors_that_sell(item)
     @vendors.find_all {|vendor| vendor.inventory.include?(item)}
   end
+
+  def sorted_item_list
+    vendor_inventory.sort.uniq
+  end
+
+  def vendor_inventory
+    @vendors.map do |vendor|
+      vendor.inventory.keys
+    end.flatten
+  end
+
+  def total_inventory
+    @vendors.each do |(item, quantity), vendor|
+      vendor.inventory.each do |item, quantity|
+        @stocked_items[item] += quantity
+      end
+    end
+    @stocked_items
+  end
+
 end
